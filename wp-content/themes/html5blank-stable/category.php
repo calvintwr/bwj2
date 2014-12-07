e922db92f266f758060be556974875a60fdb5b25
Jake@prismninjas <jake@prism-communications.com>
2014-12-07 20:48:29 +0800

    Modify category page to display to blog page

--- wp-content/themes/html5blank-stable/category.php
+++ wp-content/themes/html5blank-stable/category.php
@@ -1,19 +1,20 @@
 <?php get_header(); ?>
 
-	<main role="main">
+	<main role="main" class="container">
 		<!-- section -->
 		<section>
-
-			<h1><?php _e( 'Categories for ', 'html5blank' ); single_cat_title(); ?></h1>
-
-			<?php get_template_part('loop'); ?>
-
+			<div class="row thumbnail-holder text-left">
+		<!--	<h1><?php /*_e( 'Categories for ', 'html5blank' ); single_cat_title(); */?></h1>
+-->
+			<?php get_template_part('loop-category'); ?>
+				<div class="clearfix"></div>
+           </div>
 			<?php get_template_part('pagination'); ?>
 
 		</section>
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+<?php// get_sidebar(); ?>
 
 <?php get_footer(); ?>
