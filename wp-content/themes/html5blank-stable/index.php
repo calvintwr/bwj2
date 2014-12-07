80ae8d454cd22f58eb85b9ebca1442486d12f28b
Calvin Tan <calvin14@gmail.com>
2014-12-08 01:08:19 +0800

    responsive slider

--- wp-content/themes/html5blank-stable/index.php
+++ wp-content/themes/html5blank-stable/index.php
@@ -26,6 +26,54 @@
 			</ul>
 		</div>-->
 	</div>
+	<script>
+	(function($) {
+		var $readMoreATags = $('.da-link');
+		$readMoreATags.wrapInner('<div class="da-link-div"></div>');
+
+		var $heightTargets = $('.da-slider');
+		$('.da-img').each(function() { $heightTargets.push(this) });
+
+		//get the parameters
+		var styleStr = $('.banner-holder>div').attr('style');
+		var tempDiv = document.createElement('div');
+		tempDiv.style.cssText = styleStr;
+		var $tempDiv = $(tempDiv);
+		var heightParam = parseInt($tempDiv.css('max-height'), 10);
+		var widthParam = parseInt($tempDiv.css('max-width'), 10);
+
+		delete $tempDiv, $tempDiv;
+
+		var AR = widthParam/heightParam;
+
+		var widthBreakPoint = 640; // the width at which to start cropping
+		var minHeight = widthBreakPoint/AR;
+		console.log(minHeight);
+		$heightTargets.css('min-height', minHeight + 'px');
+
+		function sizing() {
+			var w = $(window).width();
+			var h = w / AR;
+
+			if(w > widthBreakPoint) {
+				$heightTargets.css('height', h + 'px');
+				$('.da-img').css('left', '0px');
+			} else {
+				$heightTargets.css('height', minHeight + 'px');
+				$('.da-img').css('left', (w - widthBreakPoint)/2 + 'px');
+			}
+			
+		}
+		sizing();
+
+		//bind some events
+		$(window).load(function() {
+			return sizing();
+		}).resize(function() {
+			return sizing();
+		});
+	})(jQuery);
+	</script>	
 </section>
 <?php endif; ?>
 
